ae20d4191cacec4b3b1de523a72e9beb4da0fc14
keonjukes <keonofthephanerozoiceon@gmail.com>
2022-08-15 20:33:02 -0400

    setting up state

--- src/InputField.tsx
+++ src/InputField.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const InputField = () => {
+
+    const [todo, setTodo] = useState('');
+
     return (
         <div>
             <span>
             <input title="Add Todo" placeholder="Add a todo"></input>
-            <button>Add Todo</button>
+            <button>Add Todo 💫</button>
             </span>
         </div>
     )
